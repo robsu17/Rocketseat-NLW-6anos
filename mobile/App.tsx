c9f80fdfeb09b6919c5530142a9da6ea1e0ec222
robsu17 <robson.costa134@gmail.com>
2023-05-17 20:40:51 -0300

    back-end p1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {ImageBackground, Text, View } from 'react-native';
+import {ImageBackground, Text, View, TouchableOpacity } from 'react-native';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
@@ -26,19 +26,32 @@ export default function App() {
   return (
     <ImageBackground 
       source={blurBg} 
-      className='relative flex-1 items-center bg-gray-900'
-      imageStyle={{ position: 'absolute', left: '-100%' }}
+      className='relative flex-1 items-center bg-gray-900 px-8 py-10'
+      imageStyle={{ position: 'absolute'}}
     >
       <StyledStripes className='absolute left-2'/>
-
+      
       <View className='flex-1 items-center justify-center gap-6'>
         <NlwSpacetime />
-        <View>
-          <Text className='text-gray-50'>Sua cápsula do tempo</Text>
-          <Text className='text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
+
+        <View className='space-y-2'>
+          <Text className='text-center font-tittle text-2xl leading-tight text-gray-50'>Sua cápsula do tempo</Text>
+          <Text className='text-center font-body text-base leading-relaxed text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se 
+            quiser) com o mundo!
+          </Text>
         </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className='rounded-full bg-green-500 px-5 py-2'
+        >
+          <Text className='font-alt text-sm uppercase text-black'>Cadastrar lembrança</Text>
+        </TouchableOpacity>
       </View>
 
+      <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
       <StatusBar style="light" translucent/>
     </ImageBackground>
   );
---
+++ server/src/routes/memories.ts
@@ -0,0 +1,101 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lb/prisma'
+import { z } from 'zod'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, isPublic, coverUrl } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'a46f82c5-704b-4440-9eb6-0bba52069e50',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, isPublic, coverUrl } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
+}
