699ab460ceb464f122984a05f86bf9ad8660dc45
robsu17 <robson.costa134@gmail.com>
2023-05-17 17:27:08 -0300

    mobile v1.0

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
+import {ImageBackground, Text, View } from 'react-native';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NlwSpacetime from './src/assets/nlw-spacetime-logo.svg'
+import { styled } from 'nativewind';
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className='bg-gray-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 font-bold text-5xl'>Rocketseat</Text>
+    <ImageBackground 
+      source={blurBg} 
+      className='relative flex-1 items-center bg-gray-900'
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className='absolute left-2'/>
+
+      <View className='flex-1 items-center justify-center gap-6'>
+        <NlwSpacetime />
+        <View>
+          <Text className='text-gray-50'>Sua cápsula do tempo</Text>
+          <Text className='text-gray-100'>Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
+        </View>
+      </View>
+
       <StatusBar style="light" translucent/>
-    </View>
+    </ImageBackground>
   );
 }
